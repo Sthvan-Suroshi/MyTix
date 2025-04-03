d3b72c870acd3bc74e477feddbed3cd64d917b8a
Sthvan-Suroshi <123sthavansuroshi@gmail.com>
2025-04-03 22:24:26 +0530

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import connect from "./src/config/connect.js";
+import { PORT } from "./src/config/config.js";
+
+dotenv.config();
+const app = express();
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+const start = async () => {
+  try {
+    await connect();
+    app.listen({ port: PORT, host: "0.0.0.0" }, (err) => {
+      if (err) console.log(err);
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
